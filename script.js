2c0b509e05c5f9b2735fda2693fc12ed0fa01bd0
James <jam3sstotomas@gmail.com>
2022-08-29 01:35:05 -0400

    added back to top button and js functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburgerButton = document.querySelector('#hamburger');
     const menuCloseButton = document.querySelector('#menuClose');
     const menu = document.querySelector('.slideOutNav');
+    const backToTop = document.querySelector('.backToTop');
+    const header = document.querySelector('.top');
 
     hamburgerButton.addEventListener('click', (e) => {
         menu.classList.add('open');
@@ -10,4 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
     menuCloseButton.addEventListener('click', (e) => {
         menu.classList.remove('open');
     });
+
+    window.onscroll = function () { takeMeBack() };
+
+    function takeMeBack() {
+        if (document.documentElement.scrollTop > 900) {
+            backToTop.classList.add('popUp')
+        } else {
+            backToTop.classList.remove('popUp')
+        }
+    }
+
+    backToTop.addEventListener('click', (e) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
 })
