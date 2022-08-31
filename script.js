b5de5a4bafdaacb13eee9768dab5c399414dd953
James <jam3sstotomas@gmail.com>
2022-08-31 01:44:55 -0400

    finshed styling for 768 breakpoint on contact.html, index.html, and blog.html

--- script.js
+++ script.js
@@ -4,14 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const menu = document.querySelector('.slideOutNav');
     const backToTop = document.querySelector('.backToTop');
     const header = document.querySelector('.top');
+    const closeMenu = menu.classList.contains('open');
 
     hamburgerButton.addEventListener('click', (e) => {
         menu.classList.add('open');
+        
     });
+    if (closeMenu) {
+        console.log('hello');
+        window.addEventListener('click', function (event) {
+            if (event.target != menu) {
+                console.log('bye');
+                menu.classList.remove('open');
+            }
+        })
+    }
 
     menuCloseButton.addEventListener('click', (e) => {
         menu.classList.remove('open');
     });
+
+    window.addEventListener('click', function (event) {
+        if (event.target != menu && closeMenu == true){
+            menu.classList.remove('open');
+            console.log(closeMenu)
+        }
+    })
+
 
     window.onscroll = function () { takeMeBack() };
 
